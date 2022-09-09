f4fce8941beb316dd29def45437a7ce6ff55e50f
221152992 <113028988+221152992@users.noreply.github.com>
2022-09-09 19:54:00 +1000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+<Props : TestPropBool="TestPropBool"/>
+</template>
+
+<script>
+import Props from './Props.vue'
+
+export default {
+    data(){
+        return{
+            TestPropBool: true
+        }
+    },
+    components: {
+        Props
+    }
+}
+</script>
---
+++ Props.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Props</h1>
+    <p> This is the prop: {{ TestPropBool }}</p>
+</template>
+
+<script>
+    export default {
+        Props:{
+            TestPropBool: Boolean
+        }
+    }
+</script>
